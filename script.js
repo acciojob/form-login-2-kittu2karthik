1fa6d323b6afc3157f8c8db0f1cb4555e82ca6b0
kittu2karthik <71444538+kittu2karthik@users.noreply.github.com>
2025-11-02 23:51:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const firstName = document.getElementById('firstName').value;
+	const lastName = document.getElementById('lastName').value;
+	const phoneNumber = document.getElementById('phoneNumber').value;
+	const emailID = document.getElementById('emailId').value;
+
+	alert(`First Name: ${firstName} Last Name: ${lastNmame} Phone Number: ${phoneNumber} Email ID: ${emailID}`)
+})
