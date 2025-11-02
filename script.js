e1cf8993a8088e40d4aef241202311b8c629f287
kittu2karthik <71444538+kittu2karthik@users.noreply.github.com>
2025-11-02 23:55:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-//your JS code here. If required.
 const formEl = document.querySelector('.form');
 
 formEl.addEventListener('submit', (e) => {
 	e.preventDefault();
 
-	const firstName = document.getElementById('firstName').value;
-	const lastName = document.getElementById('lastName').value;
-	const phoneNumber = document.getElementById('phoneNumber').value;
-	const emailID = document.getElementById('emailId').value;
+	const firstName = document.querySelector('input[name="First Name"]').value;
+	const lastName = document.querySelector('input[name="Last Name"]').value;
+	const phoneNumber = document.querySelector('input[name="Phone Number"]').value;
+	const emailID = document.querySelector('input[name="Email ID"]').value;
 
-	alert(`First Name: ${firstName} Last Name: ${lastNmame} Phone Number: ${phoneNumber} Email ID: ${emailID}`)
-})
+	alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailID}`);});
